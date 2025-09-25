372e79ed42ab16c39d4e766fd7e21603535b2352
Riyan <riyansayyad978@gmail.com>
2025-09-25 22:28:23 +0530

    implement badge assignment

--- constants/filter.ts
+++ constants/filter.ts
@@ -35,3 +35,32 @@ export const CollectionFilters =[
     {name:"Most Recent", value:"mostrecent"},
     {name:'Most Answered', value:"mostanswered"},
 ]
+
+
+export const BADGE_CRITERIA = {
+  QUESTION_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  QUESTION_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  TOTAL_VIEWS: {
+    BRONZE: 1000,
+    SILVER: 10000,
+    GOLD: 100000,
+  },
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -127,3 +127,33 @@ export const formatNumber = (number:number)=>{
     return number.toString();
   }
 }
+
+import BADGE_CRITERIA from "@/constants/badges";
+import { Badges } from "@/types/global";
+
+export const assignBadges = (params: { criteria: {
+  type: keyof typeof BADGE_CRITERIA;
+  count: number;
+}[] }) => {
+  const badgeCounts: Badges = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  }
+
+  const {criteria} = params;
+
+  criteria.forEach((item)=>{
+    const {type,count} = item;
+    const badgeLevels = BADGE_CRITERIA[type];
+
+    Object.keys(badgeLevels).forEach((level)=>{
+      if(count >= badgeLevels[level as keyof typeof badgeLevels]){
+        badgeCounts[level as keyof Badges] +=1;
+      }
+  })
+
+  })
+
+  return badgeCounts;
+}
